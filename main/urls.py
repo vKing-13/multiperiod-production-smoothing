5bcaccc3a34cd4742a157cb169f6ba46543e7a36
Muhammad Iqbal Habibie <habibie@graduate.utm.my>
2022-11-22 18:09:35 +0800

    Page Navigation and Template

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-  path('',views.calculate,name="calculate")
+  path('',views.calculateFinalCost,name="Calculate Final Cost"),
+  path('calculateFHC/',views.calculateFiringHiringCost,name="Calculate Firing and Hiring Cost"),
+  path('calculateIC/',views.calculateInventoryConstraints,name="Calculate Monthly Inventory Constraints"),
+  path('calculateIHC/',views.calculateInventoryHoldingCost,name="Calculate Inventory Holding Cost"),
+  path('calculateNTW/',views.calculateNumOfTempWorkers,name="Calculate Num. of Temp. Workers Monthly"),
+  path('calculateRD/',views.calculateRemainingDemand,name="Calculate Remaining Demand Monthly"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def calculate(response):
+def calculateFinalCost(response):
   return render(response,"main/calculate_final_cost.html",{})
+
+def calculateFiringHiringCost(response):
+  return render(response,"main/calculate_firing_hiring_cost.html",{})
+
+def calculateInventoryConstraints(response):
+  return render(response,"main/calculate_inventory_constraints.html",{})
+
+def calculateInventoryHoldingCost(response):
+  return render(response,"main/calculate_inventory_holding_cost.html",{})
+
+def calculateNumOfTempWorkers(response):
+  return render(response,"main/calculate_num_of_temp_workers.html",{})
+
+def calculateRemainingDemand(response):
+  return render(response,"main/calculate_remaining_demand.html",{})
