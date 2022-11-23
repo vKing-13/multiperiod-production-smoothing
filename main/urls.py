36b3f78eef77b2b124b961fa6f45aa99b0085fd9
Muhammad Iqbal Habibie <habibie@graduate.utm.my>
2022-11-23 11:30:51 +0800

    Added Index Page

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-  path('',views.calculateFinalCost,name="Calculate Final Cost"),
+  path('',views.index,name="Home Page"),
+  path('calculateFC/',views.calculateFinalCost,name="Calculate Final Cost"),
   path('calculateFHC/',views.calculateFiringHiringCost,name="Calculate Firing and Hiring Cost"),
   path('calculateIC/',views.calculateInventoryConstraints,name="Calculate Monthly Inventory Constraints"),
   path('calculateIHC/',views.calculateInventoryHoldingCost,name="Calculate Inventory Holding Cost"),
