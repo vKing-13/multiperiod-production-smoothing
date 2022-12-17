35d352187366a3a0fadde29362e00e7fe0b2a97f
Muhammad Faiq Haidar <mfaiq.haidar86@gmail.com>
2022-12-17 18:33:04 +0800

    Registration CSS completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './main/templates/*.{html,js}',
     './node_modules/flowbite/**/*.{html,js}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
