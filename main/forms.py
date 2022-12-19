0ddafcb6eaa3bee576f4593bf7a99fd46cbde8cd
Vico <vicolee1363@gmail.com>
2022-12-19 23:15:04 +0800

    remove crispy

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from .import models
 from django.contrib.auth.models import User
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Row, Column
+
 
 class BossUserForm(forms.ModelForm):
   class Meta:
@@ -17,29 +16,6 @@ class WorkerUserForm(forms.ModelForm):
   class Meta:
     model=User
     fields=['first_name','last_name','username','email', 'password']
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.helper = FormHelper()
-    self.helper.layout = Layout(
-        Row(
-            Column('first_name', css_class='pb-2 text-2xl  text-center bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-blue-500 font-bold'),
-            Column('last_name', css_class='pb-2 text-2xl  text-center bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-blue-500 font-bold'),
-
-        ),
-        #  Row(
-        #     Column('username', css_class='pb-2 text-2xl  text-center bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-blue-500 font-bold'),
-            
-        # ),
-         Row(
-            'email', css_class='pb-2 text-2xl block text-center bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-blue-500 font-bold'
-            
-        ),
-         Row(
-           'password', css_class='pb-2 text-2xl block text-center bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-blue-500 font-bold'
-            
-        ),
-        Submit('submit', 'Submit',css_class='bg-indigo-600 text-white py-2 w-full rounded-md drop-shadow-lg transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-blue-500 hover:text-white font-bold')
-    )
 class WorkerExtraForm(forms.ModelForm):
   class Meta:
     model=models.WorkerExtra
