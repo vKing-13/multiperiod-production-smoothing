66c0eb9347c688806eef1a96137c7a4deaf65fb6
Vico <vicolee1363@gmail.com>
2023-01-03 23:16:34 +0800

    delete not used files

--- main/urls.py
+++ main/urls.py
@@ -8,17 +8,16 @@ urlpatterns = [
     # path('worker-signup/',views.worker_signup_view ),
     # path('worker-login/',LoginView.as_view(template_name="main/worker_login.html"),name="worker-login"),
 
-    path('boss-signup/', views.boss_signup_view),
-    path('boss-login/', LoginView.as_view(template_name="main/boss_login.html"),
-         name="boss-login"),
+    path('login/', LoginView.as_view(template_name="main/login.html"),
+         name="login"),
 
     path('logout', views.logout_view, name='logout'),
     path('history', views.history, name='history'),
     path('formula', views.formula, name='formula'),
 
-    path('admin-login/', LoginView.as_view(template_name="main/admin_login.html"),
-         name="admin-login"),
-    path('admin_view_user/', views.admin_view_user_view, name="admin_view_user"),
+    #     path('admin-login/', LoginView.as_view(template_name="main/admin_login.html"),
+    #          name="admin-login"),
+    #     path('admin_view_user/', views.admin_view_user_view, name="admin_view_user"),
     # path('delete-boss/<int:pk>', views.delete_boss_view,name='delete-boss'),
     # path('delete-worker/<int:pk>', views.delete_worker_view,name='delete-worker'),
 
