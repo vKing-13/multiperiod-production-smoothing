2b1296368137e50272bf49926e20f347e945a920
Muhammad Iqbal Habibie <habibie@graduate.utm.my>
2023-01-03 22:41:34 +0800

    Optimization Algorithm applied to the application

--- main/models.py
+++ main/models.py
@@ -21,29 +21,28 @@ class BossExtra(models.Model):
 
 class PlanDatabase(models.Model):
     #Input
-    planName = models.CharField(max_length=200)                                 # Plan Name
-    demand1 = models.IntegerField()			                                    # Demand for Month 1
-    demand2 = models.IntegerField()			                                    # Demand for Month 2
-    demand3 = models.IntegerField()			                                    # Demand for Month 3
-    demand4 = models.IntegerField()			                                    # Demand for Month 4
-    numPermanent = models.IntegerField()		                                # Number of Permanent Worker(s)
-    prodPermanent = models.IntegerField()		                                # Production of a Permanent Worker
-    prodTemporary = models.IntegerField()		                                # Production of a Temporary Worker
-    costHiring = models.DecimalField(max_digits=19, decimal_places=2)			# Temporary Worker Hiring Cost
-    costFiring = models.DecimalField(max_digits=19, decimal_places=2)			# Temporary Worker Firing Cost
-    costHoldingUnit = models.DecimalField(max_digits=19, decimal_places=2)		# Monthly Holding Cost per Unit
-    inventoryInitial = models.IntegerField()	                                # Initial Inventory Level
-    inventoryFinal = models.IntegerField()		                                # Final Inventory Level
-    #Output
-    inventoryMonth1 = models.IntegerField()                                     # Ending Inventory for Month 1
-    inventoryMonth2 = models.IntegerField()                                     # Ending Inventory for Month 2
-    inventoryMonth3 = models.IntegerField()                                     # Ending Inventory for Month 3
-    hiredTemporary1 = models.IntegerField()                                     # Number of Temporary Worker Hired for Month 1
-    hiredTemporary2 = models.IntegerField()                                     # Number of Temporary Worker Hired for Month 2
-    hiredTemporary3 = models.IntegerField()                                     # Number of Temporary Worker Hired for Month 3
-    hiredTemporary4 = models.IntegerField()                                     # Number of Temporary Worker Hired for Month 4
-    firedTemporary1 = models.IntegerField()                                     # Number of Temporary Worker Fired for Month 1
-    firedTemporary2 = models.IntegerField()                                     # Number of Temporary Worker Fired for Month 2
-    firedTemporary3 = models.IntegerField()                                     # Number of Temporary Worker Fired for Month 3
-    firedTemporary4 = models.IntegerField()                                     # Number of Temporary Worker Fired for Month 4
-    optimalCost = models.DecimalField(max_digits=19, decimal_places=2)          # Optimized Cost for Multi-Period Planning 
+    planName = models.CharField(max_length=200)                                             # Plan Name
+    demand1 = models.IntegerField()			                                                # Demand for Month 1
+    demand2 = models.IntegerField()			                                                # Demand for Month 2
+    demand3 = models.IntegerField()			                                                # Demand for Month 3
+    demand4 = models.IntegerField()			                                                # Demand for Month 4
+    numPermanent = models.IntegerField()		                                            # Number of Permanent Worker(s)
+    prodPermanent = models.IntegerField()		                                            # Production of a Permanent Worker
+    prodTemporary = models.IntegerField()		                                            # Production of a Temporary Worker
+    costHiring = models.DecimalField(max_digits=19, decimal_places=2)			            # Temporary Worker Hiring Cost
+    costFiring = models.DecimalField(max_digits=19, decimal_places=2)			            # Temporary Worker Firing Cost
+    costHoldingUnit = models.DecimalField(max_digits=19, decimal_places=2)		            # Monthly Holding Cost per Unit
+    inventoryInitial = models.IntegerField()	                                            # Initial Inventory Level
+    inventoryFinal = models.IntegerField()		                                            # Final Inventory Level
+    inventoryMonth1 = models.IntegerField(default=0)                                        # Ending Inventory for Month 1
+    inventoryMonth2 = models.IntegerField(default=0)                                        # Ending Inventory for Month 2
+    inventoryMonth3 = models.IntegerField(default=0)                                        # Ending Inventory for Month 3
+    hiredTemporary1 = models.IntegerField(default=0)                                        # Number of Temporary Worker Hired for Month 1
+    hiredTemporary2 = models.IntegerField(default=0)                                        # Number of Temporary Worker Hired for Month 2
+    hiredTemporary3 = models.IntegerField(default=0)                                        # Number of Temporary Worker Hired for Month 3
+    hiredTemporary4 = models.IntegerField(default=0)                                        # Number of Temporary Worker Hired for Month 4
+    firedTemporary1 = models.IntegerField(default=0)                                        # Number of Temporary Worker Fired for Month 1
+    firedTemporary2 = models.IntegerField(default=0)                                        # Number of Temporary Worker Fired for Month 2
+    firedTemporary3 = models.IntegerField(default=0)                                        # Number of Temporary Worker Fired for Month 3
+    firedTemporary4 = models.IntegerField(default=0)                                        # Number of Temporary Worker Fired for Month 4
+    optimalCost = models.DecimalField(max_digits=19, decimal_places=2, default=0)           # Optimized Cost for Multi-Period Planning 
--- main/views.py
+++ main/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
 from django.contrib.auth import logout, login, authenticate
+from pulp import *
 # Create your views here.
 
 
@@ -23,7 +24,57 @@ def is_upper(user):
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    if request.method == "POST":
+        inputPlanName = str(request.POST.get('planName'))
+        inputDemand1 = int(request.POST.get('demand1'))
+        inputDemand2 = int(request.POST.get('demand2'))
+        inputDemand3 = int(request.POST.get('demand3'))
+        inputDemand4 = int(request.POST.get('demand4'))
+        inputNumPermanent = int(request.POST.get('numPermanent'))
+        inputProdPermanent = int(request.POST.get('prodPermanent'))
+        inputProdTemporary = int(request.POST.get('prodTemporary'))
+        inputCostHiring = float(request.POST.get('costHiring'))
+        inputCostFiring = float(request.POST.get('costFiring'))
+        inputCostHoldingUnit = float(request.POST.get('costHoldingUnit'))
+        inputInventoryInitial = int(request.POST.get('inventoryInitial'))
+        inputInventoryFinal = int(request.POST.get('inventoryFinal'))
+        queryCheck = models.PlanDatabase.objects.filter(planName=inputPlanName).exists()
+        if queryCheck == True:
+            inputPlanName = inputPlanName+'_copy'
+        data = {'planName':inputPlanName, 
+                'demand1':inputDemand1, 
+                'demand2':inputDemand2, 
+                'demand3':inputDemand3, 
+                'demand4':inputDemand4, 
+                'numPermanent':inputNumPermanent,
+                'prodPermanent':inputProdPermanent,
+                'prodTemporary':inputProdTemporary,
+                'costHiring':inputCostHiring,
+                'costFiring':inputCostFiring,
+                'costHoldingUnit':inputCostHoldingUnit,
+                'inventoryInitial':inputInventoryInitial, 
+                'inventoryFinal': inputInventoryFinal 
+                }
+        form = forms.PlanningForm(data)
+        form.save()
+        model = LpProblem("Minimize Cost",LpMinimize)
+        month= list(range(4))
+        ihcDict = LpVariable.dicts('IHC',month,lowBound=0, cat='Continuous')
+        ihcDict[3] = inputInventoryFinal
+        hcDict = LpVariable.dicts('HC',month,lowBound=0, cat='Continuous')
+        fcDict = LpVariable.dicts('FC',month,lowBound=0, cat='Continuous')
+        model += lpSum([inputCostHoldingUnit * ihcDict[i] for i in month]) + lpSum([inputCostHiring * hcDict[i] for i in month]) + lpSum([inputCostFiring * fcDict[i] for i in month])
+        model.addConstraint(inputInventoryInitial + inputProdTemporary*hcDict[0] - inputProdTemporary*fcDict[0] - ihcDict[0] == inputDemand1 - (inputNumPermanent * inputProdPermanent))
+        model.addConstraint(ihcDict[0] + inputProdTemporary*hcDict[0] - inputProdTemporary*fcDict[0] + inputProdTemporary*hcDict[1] - inputProdTemporary*fcDict[1] - ihcDict[1] == inputDemand2 - (inputNumPermanent * inputProdPermanent))
+        model.addConstraint(ihcDict[1] + inputProdTemporary*hcDict[0] - inputProdTemporary*fcDict[0] + inputProdTemporary*hcDict[1] - inputProdTemporary*fcDict[1] + inputProdTemporary*hcDict[2] - inputProdTemporary*fcDict[2] - ihcDict[2] == inputDemand3 - (inputNumPermanent * inputProdPermanent))
+        model.addConstraint(ihcDict[2] + inputProdTemporary*hcDict[0] - inputProdTemporary*fcDict[0] + inputProdTemporary*hcDict[1] - inputProdTemporary*fcDict[1] + inputProdTemporary*hcDict[2] - inputProdTemporary*fcDict[2] + inputProdTemporary*hcDict[3] - inputProdTemporary*fcDict[3] - ihcDict[3] == inputDemand4 - (inputNumPermanent * inputProdPermanent))
+        model.solve()
+        models.PlanDatabase.objects.filter(planName=inputPlanName).update(inventoryMonth1 = ihcDict[0].varValue, inventoryMonth2 = ihcDict[1].varValue, inventoryMonth3 = ihcDict[2].varValue, hiredTemporary1 = hcDict[0].varValue, hiredTemporary2 = hcDict[1].varValue, hiredTemporary3 = hcDict[2].varValue, hiredTemporary4 = hcDict[3].varValue, firedTemporary1 = fcDict[0].varValue, firedTemporary2 = fcDict[1].varValue, firedTemporary3 = fcDict[2].varValue, firedTemporary4 = fcDict[3].varValue, optimalCost = value(model.objective))
+        newInput = models.PlanDatabase.objects.filter(planName=inputPlanName).values()
+        return render(request,"main/displayRow.html",  {'newInput':newInput})
+    else:
+        form = forms.PlanningForm()
+    return render(request, 'main/index.html', {'form': form})
 
 
 def logout_view(request):
